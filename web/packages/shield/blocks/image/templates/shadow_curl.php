fb2e7992a66597ad2fb686cad9b195829ee4ad41
Jonathan Hartman <jhartman86@gmail.com>
2014-04-28 16:33:15 -0600

    Cleanup (removing old stuff)

--- web/packages/shield/blocks/image/templates/shadow_curl.php
+++
@@ -1,7 +0,0 @@
-<? defined('C5_EXECUTE') or die("Access Denied."); ?>
-
-<div class="boxd">
-    <div class="shadow">
-        <?php echo($controller->getContentAndGenerate()); ?>
-    </div>
-</div>

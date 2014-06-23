66c964a262b6e7774ec2e189e671a416d457d555
Jason Geiger <jg@fisherphx.com>
2014-06-23 15:13:52 -0700

    new homepage

--- web/packages/shield/themes/svalinn/home.php
+++ web/packages/shield/themes/svalinn/home.php
@@ -29,8 +29,8 @@ Loader::element('header_required'); // REQUIRED BY C5 //
 								<div class="intro-content row">
 						        	<div class="column medium-10 medium-centered">
 							        	<?php $a = new Area('Masthead'); $a->display($c); ?>
-							        	<p><a href="btn btn-lg">LEARN MORE ABOUT OUR PRIVATE CLIENT SERVICES</a></p>
-										<p><a href="" class="btn social">Follow Svalinn Private Client on facebook</a></p>
+							        	<p><a href="/privateclient" class="btn btn-lg">LEARN MORE ABOUT OUR PRIVATE CLIENT SERVICES</a></p>
+										<p><a href="https://www.facebook.com/SnakeRiverK9" target="_blank" class="btn social">Follow Svalinn Private Client on facebook</a></p>
 
 						        	</div>
 						        </div>
@@ -43,7 +43,7 @@ Loader::element('header_required'); // REQUIRED BY C5 //
 				<!-- BEGIN .name-section -->
 				<article class="container name-section">
 					<div class="row">
-						<div class="column medium-7 large-6">
+						<div class="column medium-12 large-12">
 							<div class="intro">
 								<h1>THE SVALINN NAME</h1>
 								<p>In Norse mythology Svalinn is the name of a legendary shield given by the gods. Svalinn protects Alsvin and Avakar, two steeds that pull the sun across the sky and Earth, from the sun’s full power. Svalinn is a timeless symbol of strength and ardent vigilance. It’s a name that appropriately portrays the ever-present companionship and protection that our highly trained dogs provide.</p>
@@ -58,18 +58,18 @@ Loader::element('header_required'); // REQUIRED BY C5 //
 					<div class="row">
 						<div class="column medium-11 medium-centered">
 							<h1>A Legacy Of Vigilance.</h1>
-								<hr/>
-								<p>In addition to personal protection, Svalinn has advanced the art and ability of the military working dog.</p>
-								<p><a href="btn btn-lg">LEARN MORE ABOUT OUR PROFESSIONAL SERVICES</a></p>
-								<p><a href="" class="btn social">Follow Svalinn Professional on facebook</a></p>
+							<hr/>
+							<p>In addition to personal protection, Svalinn has advanced the art<br/>and ability of the military working dog.</p>
+							<p><a href="#" class="btn btn-lg">LEARN MORE ABOUT OUR PROFESSIONAL SERVICES</a></p>
+							<p><a href="https://www.facebook.com/pages/Svalinn/451171018338883" target="_blank" class="btn social">Follow Svalinn Professional on facebook</a></p>
 	                        
 						</div>
 					</div>
 				</article>
 				<!-- END .pro-section -->
-				
-				<div class="footer-img">
-					<a href=""><img src=""/>Follow Svalinn on Instagram</a>
+				<div class="container instagram-section">
+					<div class="source"></div>
+					<p><a href="http://instagram.com/svalinn_private" target="_blank" class="btn social">Follow Svalinn on Instagram</a></p>
 				</div>
 				<!-- BEGIN .footer -->
 	            <?php Loader::packageElement('theme_footer', 'shield'); ?>

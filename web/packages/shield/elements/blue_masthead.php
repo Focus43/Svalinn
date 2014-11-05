596cd8e04e07e7114215dda4658e721972b5ff82
Jonathan Hartman <jhartman86@gmail.com>
2014-11-05 14:36:35 -0700

    Updates

--- web/packages/shield/elements/blue_masthead.php
+++ web/packages/shield/elements/blue_masthead.php
@@ -2,6 +2,8 @@
 <div class="container masthead">
 	<div class="inner"><h1><?php echo ($customTitle) ? $customTitle : $pageObj->getAttribute('meta_title'); ?></h1></div>
 </div>
+<?php if( $hideSubtitle !== true ): ?>
 <div class="container leader">
 	<div class="inner"><h2><?php echo ($customTitle) ? $customTitle : $pageObj->getAttribute('meta_description'); ?></h2></div>
 </div>
+<?php endif; ?>

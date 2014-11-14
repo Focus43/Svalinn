b810ec9581b9f55fac87750745ba76a027ad2ff6
Jonathan Hartman <jhartman86@gmail.com>
2014-11-14 10:30:23 -0700

    Footer update

--- web/packages/shield/elements/theme_footer.php
+++ web/packages/shield/elements/theme_footer.php
@@ -43,23 +43,14 @@
         </div>
         <div class="column medium-12 large-7 contact">
             <?php if(Page::getCurrentPage()->getCollectionHandle() === 'professional'): ?>
-                different stuff here
+                <a href="mailto:info@svalinn.com" class="btn btn-lg btn-contact btn-arrow uppercase">Email Us</a>
+                <div class="or fwsb">OR</div>
+                <a href="tel:1-307-200-1223" class="btn btn-lg btn-disabled uppercase">307.200.1223</a>
             <?php else: ?>
                 <a href="mailto:info@svalinn.com" class="btn btn-lg btn-contact btn-arrow uppercase">Email Us</a>
                 <div class="or fwsb">OR</div>
                 <a href="tel:1-307-200-1223" class="btn btn-lg btn-disabled uppercase">307.200.1223</a>
             <?php endif; ?>
-            <!--
-            <ul class="socialize-list">
-                <li><a href="https://www.facebook.com/pages/Svalinn/451171018338883" target="_blank"><img src="<?php echo SHIELD_IMAGES_URL; ?>svg-icons/facebook_gray.svg" /></a></li>
-                <li><a href="http://instagram.com/svalinn_private" target="_blank"><img src="<?php echo SHIELD_IMAGES_URL; ?>svg-icons/instagram_gray.svg" /></a></li>
-                <li><a href="https://twitter.com/svalinn_private" target="_blank"><img src="<?php echo SHIELD_IMAGES_URL; ?>svg-icons/twitter_gray.svg" /></a></li>
-                <li><a href="http://svalinn-private.tumblr.com/" target="_blank"><img src="<?php echo SHIELD_IMAGES_URL; ?>svg-icons/tumblr_gray.svg" /></a></li>
-            </ul>
-            -->
-            <a href="mailto:info@svalinn.com" class="btn btn-lg btn-contact btn-arrow uppercase">Email Us</a>
-            <div class="or fwsb">OR</div>
-            <a href="tel:1-307-200-1223" class="btn btn-lg btn-disabled uppercase">307.200.1223</a>
         </div>
     </div>
     <div class="row copyright">
